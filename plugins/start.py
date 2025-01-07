6391f412799759401e4115d02004ce8386d9be9b
saeedgoraya987 <saeedgoraya987@gmail.com>
2025-01-07 20:13:44 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,14 +15,16 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    image_url = "https://i.postimg.cc/GhQk8Pk8/VKHLLRIUUSFVBQB.jpg"
     rm = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton("✨ Update Channel", url="https://t.me/vj_botz")
         ]] 
     )
-    await client.send_message(
+    await client.reply_photo(
         chat_id=message.from_user.id,
-        text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        photo=image_url,
+        caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
         reply_markup=rm,
         parse_mode=enums.ParseMode.HTML
     )
